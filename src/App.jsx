19527dad800bb08379a6ddbcce035b5c9fcc0e13
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-21 13:56:38 -0400

    updating some stuff structure wise

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
 
 function App() {
 
   const [instructors, setInstructors] = useState([])
+
+  useEffect(() => {
+    getInstructors()
+  }, [])
+
+  const getInstructors = () => {
+    axios.get('http://127.0.0.1:8000/instructors/')
+  }
 
   return (
     <div className="p-5">
