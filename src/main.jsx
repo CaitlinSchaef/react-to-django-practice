fb3bc02a936db7fab7b911c237cd18187a0fade4
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-05-21 13:57:44 -0400

    updated main jsx from jasons stuff

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from 'react-router-dom'
+
+// project styles
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+import About from './About'
+import App from './App'
+import ErrorPage from './ErrorPage'
+import Header from './Header'
+import Footer from './Footer'
+
+
+function Layout() {
+  return (
+      <>
+        <Header />
+        <div id='page-content'>
+          <Outlet />
+        </div>
+        <Footer />
+      </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
